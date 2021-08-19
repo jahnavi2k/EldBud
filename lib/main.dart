e9855ce7eaedfee8ddba272b385ef7e3d91481c5
Nandini Bajaj <20uec080@lnmiit.ac.in>
2021-08-19 13:46:58 +0530

    Added Files

---
+++ lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    home: Counter(),
+  ));
+}
+
+class Counter extends StatefulWidget {
+    @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lime[200],
+     body: Stack(
+       children: [
+         Positioned.fill(
+           top: 22.0,
+           child: Image(
+           image: AssetImage('assets/bg2.jpg'),
+           fit: BoxFit.cover,
+         ),
+         ),
+         Positioned(
+           top: 22.0,
+             left: 2.0,
+             child: Column(
+               children: [
+                 Row(
+                   children: [ Padding(
+                     padding: const EdgeInsets.all(8.0),
+                     child: Container(
+                       child: CircleAvatar(
+                         radius: 38.0,
+                         backgroundColor: Colors.black,
+                         child: CircleAvatar(
+                           radius: 35.0,
+                           backgroundImage: AssetImage('assets/cir2.jpg'),
+                         ),
+                       ),
+                     ),
+                   ),
+                     SizedBox(width: 90.0,),
+                     Container(
+                       child: Padding(
+                         padding: const EdgeInsets.fromLTRB(0.0, 15.0, 150.0, 0.0),
+                         child: Text(
+                           'TITLE',
+                           style: TextStyle(
+                             fontFamily: 'BB',
+                             fontSize: 32.0,
+                             fontWeight: FontWeight.bold,
+                           ),
+                         ),
+                       ),
+                     ),
+                   ],
+                 ),
+                 SizedBox(height: 20.0,),
+                 Row(
+                   children: [
+                     Container(
+                       child: SizedBox(width: 25.0,),
+                     ),
+                     Container(
+                       child: Text(
+                         'WELCOMING TEXT MESSAGE',
+                         style: TextStyle(
+                           fontSize: 24.0,
+                           fontFamily: 'BB',
+                         ),
+                       ),
+                     )
+                   ],
+                 ),
+                 SizedBox(height: 90.0,),
+                 Row(
+                   children: [
+                     Container(
+                        child: SizedBox(width: 50.0,),
+                     ),
+                     Container(
+                       child: Padding(
+                         padding: const EdgeInsets.fromLTRB(0.0, 0.0, 50.0, 0.0),
+                         child: CircleAvatar(
+                           backgroundColor: Colors.amber,
+                           radius: 133.0,
+                           child: CircleAvatar(
+                             radius: 125.0,
+                             backgroundImage: AssetImage('assets/logo2.jpeg'),
+                           ),
+                         ),
+                       ),
+                     )
+                   ],
+                 ),
+                 SizedBox(height: 90.0,),
+                 Row(
+                   children: [
+                     Container(
+                       child: SizedBox(width: 255.0,),
+                     ),
+                     Container(
+                       child: ElevatedButton(
+                         child: Text(
+                             "     View Questions",
+                         style: TextStyle(
+                           fontSize: 20.0,
+                           fontFamily: 'Mon',
+
+                         ),
+                         ),
+                         onPressed: () {},
+                         style: ElevatedButton.styleFrom(
+                           fixedSize: Size(140.0,60.0),
+                           primary: Colors.red,
+                           onPrimary: Colors.amberAccent,
+                           shape: RoundedRectangleBorder(
+                             borderRadius: BorderRadius.circular(12.0),
+                           ),
+                         ),
+                       )
+                     )
+                   ],
+                 )
+               ],
+             )
+         ),
+       ],
+     ),
+      );
+  }
+}
+
