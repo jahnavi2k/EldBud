66bb2814c7cfd64522090a1c1285b63681757910
Nandini Bajaj <20uec080@lnmiit.ac.in>
2021-08-20 00:32:28 +0530

    Added Files

--- lib/pages/questions.dart
+++ lib/pages/questions.dart
@@ -200,6 +200,30 @@ class _QuestionsState extends State<Questions> {
                       ),
                     )
                 ),
+                SizedBox(height: 30.0,),
+                Container(
+                    child: ElevatedButton(
+                      child: Text(
+                        "Save a Contact.",
+                        style: TextStyle(
+                          fontSize: 22.0,
+                          fontFamily: 'Mon',
+
+                        ),
+                      ),
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: Size(140.0,60.0),
+                        primary: Color(0XFFE2DED0),
+                        onPrimary: Color(0XFF647C90),
+                        shadowColor: Colors.black,
+                        elevation: 50.0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                      ),
+                    )
+                ),
                 SizedBox(height: 20.0,),
               ],
             ),
