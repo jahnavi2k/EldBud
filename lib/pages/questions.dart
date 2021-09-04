d1811c124968d3bd734d51e0b7cdb9a69b2fa310
jahnavi2k <jahnavi2k@gmail.com>
2021-09-04 12:23:50 +0530

    ADDED SAVING A CONTACT YAYY!! xD

--- lib/pages/questions.dart
+++ lib/pages/questions.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:open_settings/open_settings.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:system_settings/system_settings.dart';
 import 'dart:async';
 import 'package:url_launcher/url_launcher.dart';
@@ -11,7 +12,38 @@ class Questions extends StatefulWidget {
 }
 
 class _QuestionsState extends State<Questions> {
+  Future<void> _askPermissions(String routeName) async {
+    PermissionStatus permissionStatus = await _getContactPermission();
+    if (permissionStatus == PermissionStatus.granted) {
+      if (routeName != null) {
+        Navigator.of(context).pushNamed(routeName);
+      }
+    } else {
+      _handleInvalidPermissions(permissionStatus);
+    }
+  }
 
+  Future<PermissionStatus> _getContactPermission() async {
+    PermissionStatus permission = await Permission.contacts.status;
+    if (permission != PermissionStatus.granted &&
+        permission != PermissionStatus.permanentlyDenied) {
+      PermissionStatus permissionStatus = await Permission.contacts.request();
+      return permissionStatus;
+    } else {
+      return permission;
+    }
+  }
+
+  void _handleInvalidPermissions(PermissionStatus permissionStatus) {
+    if (permissionStatus == PermissionStatus.denied) {
+      final snackBar = SnackBar(content: Text('Access to contact data denied'));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    } else if (permissionStatus == PermissionStatus.permanentlyDenied) {
+      final snackBar =
+      SnackBar(content: Text('Contact data not available on device'));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+  }
   Future<void>? _launched;
 
   Future<void> _launchInBrowser(String url) async {
@@ -57,16 +89,14 @@ class _QuestionsState extends State<Questions> {
               Container(
                   child: ElevatedButton(
                     child: Text(
-                      "Contacts",
+                      "Save a Contact",
                       style: TextStyle(
                         fontSize: 22.0,
                         fontFamily: 'Mon',
 
                       ),
                     ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, 'contactlist');
-                    },
+                    onPressed: () => _askPermissions('add'),
                     style: ElevatedButton.styleFrom(
                       fixedSize: Size(140.0,60.0),
                       primary: Color(0xFF07224C),
@@ -294,7 +324,7 @@ class _QuestionsState extends State<Questions> {
               Container(
                   child: ElevatedButton(
                     child: Text(
-                      "Save a Contact",
+                      "Accessibilty",
                       style: TextStyle(
                         fontSize: 22.0,
                         fontFamily: 'Mon',
